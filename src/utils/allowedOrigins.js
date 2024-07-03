1cec0c6a3c58d13d0e8f97aca07736684e71fb9e
danishap07 <81384326+Danishap07@users.noreply.github.com>
2024-07-04 00:39:32 +0530

    allow

--- src/utils/allowedOrigins.js
+++ src/utils/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
     'http://localhost:3000',
     'localhost:3000',
-    "https://7d-living-frontend.vercel.app/"
+    'https://7d-living-frontend.vercel.app',
+    '7d-living-frontend.vercel.app'
 ]
 
 module.exports = allowedOrigins;
